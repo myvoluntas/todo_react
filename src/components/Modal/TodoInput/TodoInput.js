632376cba12e76285e1bdf75306bfa47bba75f9f
Guber <myvoluntas@users.noreply.github.com>
2020-04-26 16:47:48 +0200

    Init Project as group on codesandbox

---
+++ src/components/Modal/TodoInput/TodoInput.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoInput = props => {
+  return <div />;
+};
+
+export default TodoInput;
---
+++ src/components/TodoItem/TodoItems.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoItems = props => {
+  return <div />;
+};
+
+export default TodoItems;
